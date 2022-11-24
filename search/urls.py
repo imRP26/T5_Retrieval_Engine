e141181e9f1eb4c589122d72b799cf37a627cca1
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2022-11-24 17:08:05 +0530

    Added image search functionality

--- search/urls.py
+++ search/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search', views.search, name='index')
+    path('search', views.search, name='index'),
+    path('imageSearch', views.imageSearch, name='imageSearch')
 ]
--- search/views.py
+++ search/views.py
@@ -25,7 +25,34 @@ def search(request):
         context = {
             'final_result': final_result
         }
-        print(len(final_result))
         return render(request, 'search.html', context)
     else:
         return render(request, 'search.html')
+
+def imageSearch(request):
+    if request.method == 'POST':
+        search = request.POST['imageSearch']
+        max_pages = 5
+        final_result = []
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',}
+        params = {
+            "q": search,
+            "tbm": "isch",                # image results
+            "hl": "en",                   # language of the search
+            "gl": "us",                   # country where search comes from
+            "ijn": "0",                    # page number
+        }
+        images = []
+        for page in range(0, max_pages):
+            params['ijn'] = str(page)
+            html = requests.get("https://www.google.com/search", params=params, headers=headers, timeout=30)
+            soup = bs(html.text, "lxml")
+            for img in soup.select("img"):
+                images.append(img['src'])
+        context = {
+            'images': images,
+            'query': search
+        }
+        return render(request, 'imageSearch.html', context)
+    else:
+        return render(request, 'imageSearch.html')
